d7d7fe0a18e8db5ddec9b142d3bd4ae5ceced005
ecekic <64327424+ecekic@users.noreply.github.com>
2021-11-06 20:02:40 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,20 +1,19 @@
 function countWords() {
-      
-        var words = document.getElementById("word").value;
-
-
-        var count = 0;
-
-      
-        var split_words = words.split(" ");
-
         
-        for (var i = 0; i < split_words.length; i++) {
-          if (split_words[i] != "") {
-            count++;
-          }
-        }
+      // each word we input is stored in this variable  
+      var words = document.getElementById("word").value;
 
-        
-        document.getElementById("show").innerHTML = count;
+      var count = 0;
+
+      // new variable will have each word separated by a space
+      var split_words = words.split(" ");
+
+      // counting each word
+      for (var i = 0; i < split_words.length; i++) {
+            if (split_words[i] != "") {
+                  count++;
+            }
       }
+      // this is how we display the count variable, showing how many words were inputted 
+      document.getElementById("show").innerHTML = count;
+}
